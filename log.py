a3287cda9847ec28db210afaf0fe5829e5dadafe
Michael Chang <mbchang2017@gmail.com>
2019-07-21 18:56:22 -0700

    base logger

--- log.py
+++ log.py
@@ -1,8 +1,9 @@
 import copy
-
+import datetime
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import os
 
 class RunningAverage(object):
     def __init__(self):
@@ -23,8 +24,70 @@ class RunningAverage(object):
         else:
             assert KeyError
 
-class VickreyLogger(object):
-    def __init__(self, env):
+class BaseLogger(object):
+    def __init__(self, root, expname, setdate):
+        self.root = root
+        self.expname = expname
+        self.logdir = self.create_logdir(root=self.root, expname=self.expname, setdate=setdate)
+
+        self.data = {}
+        self.metrics = {}
+
+        self.run_avg = RunningAverage()
+
+    def create_logdir(self, root, expname, setdate):
+        self.logdir = os.path.join(root, expname)
+        if setdate:
+            if not expname == '': self.logdir += '-'
+            self.logdir += '{date:%Y-%m-%d_%H-%M-%S}'.format(
+            date=datetime.datetime.now())
+        os.mkdir(self.logdir)
+        return self.logdir
+
+    def add_variables(self, names):
+        for name in names:
+            self.add_variable(name)
+
+    def add_variable(self, name, include_running_avg=False):
+        self.data[name] = []
+        if include_running_avg:
+            self.data['running_{}'.format(name)] = []
+
+    def update_variable(self, name, index, value, include_running_avg=False):
+        if include_running_avg:
+            running_name = 'running_{}'.format(name)
+            self.run_avg.update_variable(name, value)
+            self.data[running_name].append((index, self.run_avg.get_value(name)))
+        self.data[name].append((index, value))
+
+    def get_recent_variable_value(self, name):
+        index, recent_value = copy.deepcopy(self.data[name][-1])
+        return recent_value
+
+    def has_running_avg(self, name):
+        return self.run_avg.exists(name)
+
+    def add_metric(self, name, initial_val, comparator):
+        self.metrics[name] = {'value': initial_val, 'cmp': comparator}
+
+    def plot(self, var_pairs):
+        for var1_name, var2_name in var_pairs:
+
+            x_indices, x_values = zip(*self.data[var1_name])
+            y_indices, y_values = zip(*self.data[var2_name])
+
+            fname = '{}_{}'.format(self.expname, var2_name)
+            plt.plot(x_values,y_values)
+            plt.xlabel(var1_name)
+            plt.ylabel(var2_name)
+            plt.savefig(os.path.join(self.logdir,'{}.png'.format(fname)))
+            plt.close()
+
+class VickreyLogger(BaseLogger):
+    def __init__(self, env, root, expname, setdate):
+        super(VickreyLogger, self).__init__(root, expname, setdate)
+        # so if you want to convert this to HDF5 you will need to pre-compute the size
+        # so maybe a low priority for now
         self.i_episodes = []
         self.payoffs = {s: [] for s in env.states}
         self.bids = {s: [] for s in env.states}
@@ -50,5 +113,8 @@ class VickreyLogger(object):
 
         plt.title(title)
         plt.tight_layout()
-        plt.savefig('{}_bootstrap.png'.format(title.replace(' ', '_')))
+        fname = '{}.png'.format(title.replace(' ', '_'))
+        plt.savefig(os.path.join(self.logdir, fname))
         plt.close()  # plt.clf() shrinks the plot for some reason
+
+
