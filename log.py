344154a2a78b02ed28eb7c8d643e7c2b29ddfa74
Michael Chang <mbchang2017@gmail.com>
2019-08-10 00:29:03 -0700

    MultibaseLogger and envLogger

--- log.py
+++ log.py
@@ -33,17 +33,51 @@ class RunningAverage(object):
         else:
             assert KeyError
 
-class BaseLogger(object):
-    def __init__(self, args):
+# class BaseLogger(object):
+#     def __init__(self, args):
+#         self.args = args
+#         self.root = args.root
+#         self.expname = args.expname
+#         self.logdir = self.create_logdir(root=self.root, expname=self.expname, setdate=True)
+
+#     def printf(self, string):
+#         if self.args.printf:
+#             f = open(os.path.join(self.logdir, self.expname+'.txt'), 'a')
+#             print(string, file=f)
+#         else:
+#             print(string)
+
+#     def pprintf(self, string):
+#         if self.args.printf:
+#             f = open(os.path.join(self.logdir, self.expname+'.txt'), 'a')
+#             pprint.pprint(string, stream=f)
+#         else:
+#             pprint.pprint(string)
+
+#     def create_logdir(self, root, expname, setdate):
+#         self.logdir = os.path.join(root, expname)
+#         if setdate:
+#             if not expname == '': self.logdir += '-'
+#             self.logdir += '{date:%Y-%m-%d_%H-%M-%S}'.format(
+#             date=datetime.datetime.now())
+#         os.mkdir(self.logdir)
+#         return self.logdir
+
+#     def remove_logdir(self):
+#         should_remove = input('Remove {}? [y/n] '.format(self.logdir))
+#         if should_remove == 'y':
+#             shutil.rmtree(self.logdir)
+#             print('Removed {}'.format(self.logdir))
+#         else:
+#             print('Did not remove {}'.format(self.logdir))
+
+class MultiBaseLogger(object):
+    def __init__(self, loggers, args):
+        self.loggers = loggers
         self.args = args
         self.root = args.root
         self.expname = args.expname
         self.logdir = self.create_logdir(root=self.root, expname=self.expname, setdate=True)
-
-        self.data = {}
-        self.metrics = {}
-
-        self.run_avg = RunningAverage()
 
     def printf(self, string):
         if self.args.printf:
@@ -76,9 +110,28 @@ class BaseLogger(object):
         else:
             print('Did not remove {}'.format(self.logdir))
 
-    def add_variables(self, names):
-        for name in names:
-            self.add_variable(name)
+    def add_variable(self, env_name, name, incl_run_avg=False, metric=None):
+        self.loggers[env_name].add_variable(name, incl_run_avg, metric)
+
+    def update_variable(self, env_name, name, index, value, include_running_avg=False):
+        self.loggers[env_name].update_variable(name, index, value, include_running_avg)
+
+    def get_recent_variable_value(self, env_name, name):
+        self.loggers[env_name].get_recent_variable_value(name)
+
+    def add_metric(self, env_name, name, initial_val, comparator):
+        self.loggers[env_name].add_metric(name, initial_val, comparator)
+
+    def plot(self, env_name, var_pairs, logdir, expname):
+        self.loggers[env_name].plot(var_pairs, logdir, expname)
+
+
+class EnvLogger(object):
+    def __init__(self, args):
+        super(EnvLogger, self).__init__()
+        self.data = {}
+        self.metrics = {}
+        self.run_avg = RunningAverage()
 
     def add_variable(self, name, incl_run_avg=False, metric=None):
         self.data[name] = []
@@ -107,13 +160,13 @@ class BaseLogger(object):
     def add_metric(self, name, initial_val, comparator):
         self.metrics[name] = {'value': initial_val, 'cmp': comparator}
 
-    def plot(self, var_pairs):
+    def plot(self, var_pairs, logdir, expname):
         for var1_name, var2_name in var_pairs:
             x_indices, x_values = zip(*self.data[var1_name])
             y_indices, y_values = zip(*self.data[var2_name])
-            fname = '{}_{}'.format(self.expname, var2_name)
+            fname = '{}_{}'.format(expname, var2_name)
             plt.plot(x_values,y_values)
             plt.xlabel(var1_name)
             plt.ylabel(var2_name)
-            plt.savefig(os.path.join(self.logdir,'{}.png'.format(fname)))
+            plt.savefig(os.path.join(logdir, '{}.png'.format(fname)))
             plt.close()
