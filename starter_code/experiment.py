eaac8f80d501d07f89d6f72e10f19914fc0ddde5
Michael Chang <mbchang2017@gmail.com>
2019-09-14 13:00:29 -0700

    rename agent to organism

--- starter_code/experiment.py
+++ starter_code/experiment.py
@@ -8,16 +8,17 @@ from log import RunningAverage
 import ipdb
 from tqdm import tqdm
 
-import env_utils as eu
+import starter_code.env_utils as eu
 
 class Experiment():
     def __init__(self, agent, task_progression, rl_alg, logger, device, args):
-        self.agent = agent
+        self.organism = agent
         self.task_progression = task_progression
         self.rl_alg = rl_alg
         self.logger = logger
         self.device = device
         self.args = args
+        self.epoch = 0
 
     def get_bids_for_episode(self, episode_data):
         episode_bids = defaultdict(lambda: [])
@@ -38,7 +39,7 @@ class Experiment():
         for t in range(self.rl_alg.max_buffer_size):  # Don't infinite loop while learning
             state_var = torch.from_numpy(state).float().unsqueeze(0).to(self.device)
             with torch.no_grad():
-                action_dict = self.agent(state_var, deterministic=deterministic)
+                action_dict = self.organism(state_var, deterministic=deterministic)
             next_state, reward, done, _ = env.step(action_dict['action'])
             #################################################
             # Debugging MiniGrid
@@ -57,7 +58,7 @@ class Experiment():
                 frame = eu.render(env=env, scale=0.25)
                 e['frame'] = frame
             episode_data.append(e)
-            self.agent.store_transition(e)
+            self.organism.store_transition(e)
             if done:
                 break
             state = next_state
@@ -71,7 +72,7 @@ class Experiment():
         all_episodes_data = []
 
         while num_steps < self.rl_alg.max_buffer_size:
-            train_env_manager = self.task_progression.sample(i=0, mode='train')
+            train_env_manager = self.task_progression.sample(i=self.epoch, mode='train')
             episode_data, episode_stats= self.sample_trajectory(env=train_env_manager.env, deterministic=deterministic, render=False)
             all_episodes_data.append(episode_stats)
             num_steps += (episode_stats['steps'])
@@ -90,22 +91,21 @@ class Experiment():
         run_avg = RunningAverage()
         for epoch in range(max_epochs):
             if epoch % self.args.eval_every == 0:
-                # stats = self.eval_env(env_manager=self.env_manager, epoch=epoch)
-                stats = self.eval_env(epoch=epoch)
+                stats = self.eval(epoch=epoch)
                 self.logger.saver.save(epoch, 
                     {'args': self.args,
                      'logger': self.logger.get_state_dict(),
                      'experiment': stats,
-                     'agent': self.agent.get_state_dict()})
+                     'agent': self.organism.get_state_dict()})
 
             episode_data, stats = self.collect_samples(deterministic=False)
             running_return = run_avg.update_variable('reward', stats['avg_return'])
 
             if epoch >= self.args.anneal_policy_lr_after:
-                self.agent.step_optimizer_schedulers(self.logger.printf)
+                self.organism.step_optimizer_schedulers(self.logger.printf)
 
             if epoch % self.args.update_every == 0:
-                self.rl_alg.improve(self.agent)
+                self.rl_alg.improve(self.organism)
 
             if epoch % self.args.log_every == 0:
                 self.logger.printf('Episode {}\tAvg Return: {:.2f}\tMin Return: {:.2f}\tMax Return: {:.2f}\tRunning Return: {:.2f}'.format(
@@ -118,7 +118,7 @@ class Experiment():
                 episode_data, stats = self.sample_trajectory(env=env_manager.env, deterministic=False, render=visualize)
                 ret = stats['return']
 
-                if i == 0 and visualize and self.agent.policy.discrete:
+                if i == 0 and visualize and self.organism.policy.discrete:
                     bids = self.get_bids_for_episode(episode_data)
                     env_manager.save_video(epoch, i, bids, ret, episode_data)
             returns.append(ret)
@@ -130,9 +130,9 @@ class Experiment():
                  'max_return': np.max(returns)}
         return stats
 
-    def eval_env(self, epoch):
+    def eval(self, epoch):
         for mode in ['train']:
-            for env_manager in self.task_progression[0][mode]:  # epoch=0 is hardcoded!!
+            for env_manager in self.task_progression[self.epoch][mode]:  # epoch=0 is hardcoded!!
                 stats = self.test(epoch, env_manager, num_test=10, visualize=True)
                 env_manager.update_variable(name='epoch', index=epoch, value=epoch)
                 for metric in ['min_return', 'max_return', 'mean_return', 'std_return']:
