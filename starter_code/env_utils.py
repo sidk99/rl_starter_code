524f916d5f5098f2f716ce1602c97f48f100773d
Michael Chang <mbchang2017@gmail.com>
2020-01-02 11:02:38 -0500

    reshape only if BabyAI or Minigrid

--- starter_code/env_utils.py
+++ starter_code/env_utils.py
@@ -1,9 +1,12 @@
 import cv2
+from gym_minigrid.wrappers import ImgObsWrapper
 
 def render(env, scale):
     frame = env.render(mode='rgb_array')
     if frame is not None:
         h, w, c = frame.shape
-        frame = frame.reshape(w, h, c)
+        if isinstance(env, ImgObsWrapper):
+            # reshape for minigrid and babyai
+            frame = frame.reshape(w, h, c)
         frame = cv2.resize(frame, dsize=(int(h*scale), int(w*scale)), interpolation=cv2.INTER_CUBIC)
         return frame
