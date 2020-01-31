4908817e89863be31a9d94d39c4b73d9a3afc05a
Michael Chang <mbchang2017@gmail.com>
2020-01-30 23:55:42 -0500

    fix lunar lander rendering

--- starter_code/env_utils.py
+++ starter_code/env_utils.py
@@ -2,20 +2,20 @@ import cv2
 from gym_minigrid.wrappers import ImgObsWrapper
 from gym.wrappers.time_limit import TimeLimit
 from gym.envs.atari.atari_env import AtariEnv
+from gym.envs.box2d.lunar_lander import LunarLander
 
 
 def render(env, scale):
     frame = env.render(mode='rgb_array')
+
     if frame is not None:
         h, w, c = frame.shape
         if isinstance(env.env, ImgObsWrapper):
             # reshape for minigrid and babyai
             frame = frame.reshape(w, h, c)
 
-        elif isinstance(env, TimeLimit):
-            from gym.envs.box2d.lunar_lander import LunarLander
-            if isinstance(env.env, LunarLander):
-                h, w = w, h
+        if isinstance(env.env, LunarLander):
+            h, w = w, h
 
         elif isinstance(env.env, AtariEnv):
             h, w = w, h
